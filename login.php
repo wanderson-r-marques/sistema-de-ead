d1df4ed9cb199b7ccd5df2044d6da1020d915de9
Wanderson R Marques <wanderson.r.marques@gmail.com>
2021-04-13 10:09:30 -0300

    Criptografia de senha realizada

--- login.php
+++ login.php
@@ -12,39 +12,50 @@ if ($_POST['acessar'] == 's' && $_POST['token'] == '8s0dfg7s6grogpsfgsgs-*sgsfg'
         $senha = $_POST['senha'];
         $ipPc = $_SERVER["REMOTE_ADDR"];
 
+        //  Atualiza a senha para criptografia
+        if (strlen($senha) < 20) {
+            $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+            $query = "UPDATE entidades SET SENHA = :senha WHERE CPF = :cpf";
+            $smtp = $con->prepare($query);
+            $smtp->bindParam(':senha', $senhaHash, PDO::PARAM_STR);
+            $smtp->bindParam(':cpf', $cpf, PDO::PARAM_STR);
+            $smtp->execute();
+        }
+
         $query = "SELECT
-    `NOME`,
-    `NOME_FANTASIA`,
-    `CPF`,
-    `RG`,
-    `DATA_NASCIMENTO`,
-    `TELEFONE1`,
-    `TELEFONE2`,
-    `EMAIL`,
-    `PK_TIPO_CADASTRO`,
-    `MATRICULA`,
-    `SENHA`,
-    `COD_INEP`
-  FROM
-    `entidades`
-  WHERE
-     CPF = :cpf AND SENHA = :senha";
+                        `NOME`,
+                        `NOME_FANTASIA`,
+                        `CPF`,
+                        `RG`,
+                        `DATA_NASCIMENTO`,
+                        `TELEFONE1`,
+                        `TELEFONE2`,
+                        `EMAIL`,
+                        `PK_TIPO_CADASTRO`,
+                        `MATRICULA`,
+                        `SENHA`,
+                        `COD_INEP`
+                    FROM
+                        `entidades`
+                    WHERE
+                        CPF = :cpf";
 
         $smtp = $con->prepare($query);
         $smtp->bindParam(':cpf', $cpf, PDO::PARAM_STR);
-        $smtp->bindParam(':senha', $senha, PDO::PARAM_STR);
 
         if ($smtp->execute()) {
             if ($smtp->rowCount() > 0) {
-                $linha = $smtp->fetch(PDO::FETCH_OBJ);                
-                $_SESSION['cpf'] = $cpf;
+                $linha = $smtp->fetch(PDO::FETCH_OBJ);
+                if (password_verify($senha, $linha->SENHA)) {
+                    $_SESSION['cpf'] = $cpf;
+                    if ($linha->PK_TIPO_CADASTRO == 2) {
+                        header('Location: professor/painel.php');
+                    } else {
+                        header('Location: aluno/painel.php');
+                    }
 
-                if($linha->PK_TIPO_CADASTRO == 2)
-                    header('Location: professor/painel.php');
-                else
-                    header('Location: aluno/painel.php');
-
-                exit;
+                    exit;
+                }
             } else {
                 session_start();
                 $_SESSION['msg'] = "Não tem usuário cadastrado com essas credenciais!#danger";
