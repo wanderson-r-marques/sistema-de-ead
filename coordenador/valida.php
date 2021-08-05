bda305374ffef58f960f320a6421cf8266bf42e4
Wanderson R Marques <wanderson.r.marques@gmail.com>
2021-08-05 10:15:31 -0300

    Diretor: Cadastro de currículo concluido

--- coordenador/valida.php
+++ coordenador/valida.php
@@ -7,23 +7,23 @@ if (isset($_SESSION['cpf']) && $_SESSION['cpf'] != '') {
     $cpf = $_SESSION['cpf'];
     $con = conectar();
 
-    
+
 
     $query = "SELECT * FROM `entidades` e 
     JOIN tipo_cadastro tc ON e.PK_TIPO_CADASTRO = tc.PK_TIPO_CADASTRO
-    WHERE e.CPF = :cpf AND e.PK_TIPO_CADASTRO = 4";
+    WHERE e.CPF = :cpf AND e.PK_TIPO_CADASTRO = 3";
     $smtp = $con->prepare($query);
     $smtp->bindParam(':cpf', $cpf);
 
-    if ($smtp->execute()) {  
-        $entidade = $smtp->fetch(PDO::FETCH_OBJ);    
+    if ($smtp->execute()) {
+        $entidade = $smtp->fetch(PDO::FETCH_OBJ);
         $foto = $entidade->FOTO;
-        if($foto == '')
-            $foto = 'semFoto.jpg';            
+        if ($foto == '')
+            $foto = 'semFoto.jpg';
         if ($smtp->rowCount() <= 0) {
             $_SESSION['msg'] = "Sua sessão expirou!#danger";
             header('Location: ' . $url);
-        } 
+        }
     } else {
         $_SESSION['msg'] = "Sua sessão expirou!#danger";
         header('Location: ' . $url);
